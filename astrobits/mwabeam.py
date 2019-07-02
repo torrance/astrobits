b9c034b43f3058c9fc576b750d54b1ed2490ba4d
Torrance Hodgson <torrance123@gmail.com>
2019-07-02 10:57:45 +0800

    mwabeam: add caching, multithreading over frequency

--- astrobits/mwabeam.py
+++ astrobits/mwabeam.py
@@ -14,6 +14,7 @@ import mwa_pb.beam_full_EE as beam_full_EE
 import mwa_pb.primary_beam as pb
 from numba import njit, float64, int64, prange
 import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 
 from astrobits.coordinates import radec_to_altaz
 
@@ -30,68 +31,88 @@ class MWABeam(object):
         delays = [int(d) for d in metafits['DELAYS'].split(',')]
         self.delays = [delays, delays] # Shuts up mwa_pb
         self.location = mwa_pb.config.MWAPOS
+        self.rgi_cache = {}
 
     def jones(self, ras, decs, freq, time=None):
         if time is None:
             time = self.time
 
+        t0 = tm.time()
         alt, az = radec_to_altaz(ras, decs, time, self.location)
+        print("Altaz elapsed: %g" % (tm.time() - t0))
         with threadlock:
             return pb.MWA_Tile_full_EE(np.pi/2 - alt, az, freq, delays=self.delays, jones=True)
 
-    def joness(self, ras, decs, freq, time=None):
+    def joness(self, ras, decs, freqs, time=None):
         if time is None:
             time = self.time
 
         t0 = tm.time()
         alt, az = radec_to_altaz(ras, decs, time, self.location)
         za = np.pi / 2 - alt
-
-        # Calculate Jones vector across grid
-        with threadlock:
-            tile = beam_full_EE.get_AA_Cached(target_freq_Hz=freq)
-            beam = beam_full_EE.Beam(tile, self.delays, amps=np.ones([2, 16]))
-            grid_zas = np.radians(np.linspace(0, 90, 5 * 90 + 1))
-            grid_azs = np.radians(np.linspace(0, 360, 5 * 360 + 1))
-
-            gridded_jones = beam.get_FF(grid_azs, grid_zas, grid=True)  # [2, 2, alt, az]
-            gridded_jones = np.transpose(gridded_jones, [2, 3, 0, 1])  # [alt, az, 2, 2]
-        print("Gridding beam elapsed: %g" % (tm.time() - t0)); sys.stdout.flush()
+        print("Altaz elapsed: %f" % (tm.time() - t0))
 
         # Interpolate Jones vector onto our points
-        jones = np.zeros((len(ras), 2, 2), dtype=np.complex)
+        jones = np.zeros((len(ras), len(freqs), 2, 2), dtype=np.complex)
 
-        def _thread(start, end):
+        def _thread(k, freq):
+            rgi = self.get_rgi(freq)
+
             for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
-                jones[start:end, i, j] += RegularGridInterpolator(
-                    (grid_azs, grid_zas),
-                    gridded_jones[:, :,  i, j].real,
-                    method='linear',
-                    bounds_error=False,
-                    fill_value=0,
-                )(np.array((az[start:end], za[start:end])).T)
+                jones[:, k, i, j] += rgi[i][j][0](
+                    np.array((az, za)).T
+                )
 
-                jones[start:end, i, j] += 1j * RegularGridInterpolator(
-                    (grid_azs, grid_zas),
-                    gridded_jones[:, :,  i, j].imag,
-                    method='linear',
-                    bounds_error=False,
-                    fill_value=0,
-                )(np.array((az[start:end], za[start:end])).T)
+                jones[:, k, i, j] += 1j * rgi[i][j][1](
+                    np.array((az, za)).T
+                )
 
         t0 = tm.time()
-        batch = 100000
         pool = Pool()
-        for start in range(0, len(ras), batch):
-            end = start + batch
-            pool.apply_async(_thread, (start, end))
-            #_thread(start, end)
+        for k, freq in enumerate(freqs):
+            pool.apply_async(_thread, (k, freq))
+            #_thread(k, freq)
 
         pool.close()
         pool.join()
         print("Interpolating beam points elapsed: %g" % (tm.time() - t0)); sys.stdout.flush()
 
         return jones
+
+    def get_rgi(self, freq):
+        # Calculate Jones vector across grid
+        try:
+            # Try to see if we've cached the interpolator already
+            return self.rgi_cache[freq]
+        except KeyError:
+            with threadlock:
+                tile = beam_full_EE.get_AA_Cached(target_freq_Hz=freq)
+                beam = beam_full_EE.Beam(tile, self.delays, amps=np.ones([2, 16]))
+                grid_zas = np.radians(np.linspace(0, 90, 10 * 90 + 1))
+                grid_azs = np.radians(np.linspace(0, 360, 10 * 360 + 1))
+
+                gridded_jones = beam.get_FF(grid_azs, grid_zas, grid=True)  # [2, 2, alt, az]
+                gridded_jones = np.transpose(gridded_jones, [2, 3, 0, 1])  # [alt, az, 2, 2]
+
+                rgi = [[[], []], [[], []]]
+                for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+                    rgi[i][j].append(RegularGridInterpolator(
+                        (grid_azs, grid_zas),
+                        gridded_jones[:, :,  i, j].real,
+                        method='linear',
+                        bounds_error=False,
+                        fill_value=0,
+                    ))
+                    rgi[i][j].append(RegularGridInterpolator(
+                        (grid_azs, grid_zas),
+                        gridded_jones[:, :,  i, j].imag,
+                        method='linear',
+                        bounds_error=False,
+                        fill_value=0,
+                    ))
+
+                self.rgi_cache[freq] = rgi
+                return rgi
 
 
 def minimalmap(ras, decs, radius):
